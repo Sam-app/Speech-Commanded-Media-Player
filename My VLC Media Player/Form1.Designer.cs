aed3361d21387c2f4b337f5af2ef1f77650a056d
Sam-app <Samuel.meb@icloud.com>
2015-11-01 12:47:00 +0000

    Speech command thread safety not solved

---
+++ My VLC Media Player/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace My_VLC_Media_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLoadFiles = new System.Windows.Forms.Button();
+            this.btnPlayAll = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Location = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSpeechEnable = new System.Windows.Forms.Button();
+            this.mediaStatus = new System.Windows.Forms.Label();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.appToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.developerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.menuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadFiles
+            // 
+            this.btnLoadFiles.Location = new System.Drawing.Point(-3, 147);
+            this.btnLoadFiles.Name = "btnLoadFiles";
+            this.btnLoadFiles.Size = new System.Drawing.Size(311, 57);
+            this.btnLoadFiles.TabIndex = 0;
+            this.btnLoadFiles.Text = "Load Files";
+            this.btnLoadFiles.UseVisualStyleBackColor = true;
+            this.btnLoadFiles.Click += new System.EventHandler(this.btnLoadFiles_Click);
+            // 
+            // btnPlayAll
+            // 
+            this.btnPlayAll.Location = new System.Drawing.Point(0, 610);
+            this.btnPlayAll.Name = "btnPlayAll";
+            this.btnPlayAll.Size = new System.Drawing.Size(311, 59);
+            this.btnPlayAll.TabIndex = 1;
+            this.btnPlayAll.Text = "Play All";
+            this.btnPlayAll.UseVisualStyleBackColor = true;
+            this.btnPlayAll.Click += new System.EventHandler(this.btnPlayAll_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Title,
+            this.Location});
+            this.listView1.Location = new System.Drawing.Point(0, 210);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(308, 394);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // Title
+            // 
+            this.Title.Text = "Title";
+            this.Title.Width = 127;
+            // 
+            // Location
+            // 
+            this.Location.Text = "Location";
+            this.Location.Width = 176;
+            // 
+            // btnSpeechEnable
+            // 
+            this.btnSpeechEnable.AutoSize = true;
+            this.btnSpeechEnable.BackColor = System.Drawing.Color.Transparent;
+            this.btnSpeechEnable.Location = new System.Drawing.Point(-3, 78);
+            this.btnSpeechEnable.Name = "btnSpeechEnable";
+            this.btnSpeechEnable.Size = new System.Drawing.Size(311, 63);
+            this.btnSpeechEnable.TabIndex = 4;
+            this.btnSpeechEnable.Text = "Speech Command";
+            this.btnSpeechEnable.UseVisualStyleBackColor = false;
+            this.btnSpeechEnable.Click += new System.EventHandler(this.btnSpeechEnable_Click);
+            // 
+            // mediaStatus
+            // 
+            this.mediaStatus.AllowDrop = true;
+            this.mediaStatus.AutoSize = true;
+            this.mediaStatus.Location = new System.Drawing.Point(329, 639);
+            this.mediaStatus.Name = "mediaStatus";
+            this.mediaStatus.Size = new System.Drawing.Size(34, 29);
+            this.mediaStatus.TabIndex = 6;
+            this.mediaStatus.Text = "...";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.AutoSize = false;
+            this.menuStrip2.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip2.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(36, 36);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.appToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1089, 47);
+            this.menuStrip2.TabIndex = 9;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // appToolStripMenuItem
+            // 
+            this.appToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.appToolStripMenuItem.Name = "appToolStripMenuItem";
+            this.appToolStripMenuItem.Size = new System.Drawing.Size(82, 43);
+            this.appToolStripMenuItem.Text = "App";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(186, 44);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(186, 44);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.developerToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(109, 43);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // developerToolStripMenuItem
+            // 
+            this.developerToolStripMenuItem.Name = "developerToolStripMenuItem";
+            this.developerToolStripMenuItem.Size = new System.Drawing.Size(298, 42);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(317, 78);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(743, 558);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::My_VLC_Media_Player.Properties.Resources.background_image;
+            this.ClientSize = new System.Drawing.Size(1089, 671);
+            this.Controls.Add(this.mediaStatus);
+            this.Controls.Add(this.btnSpeechEnable);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnPlayAll);
+            this.Controls.Add(this.btnLoadFiles);
+            this.Controls.Add(this.menuStrip2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sam Media Player";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFiles;
+        private System.Windows.Forms.Button btnPlayAll;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Title;
+        private System.Windows.Forms.ColumnHeader Location;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btnSpeechEnable;
+        private System.Windows.Forms.Label mediaStatus;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem appToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem developerToolStripMenuItem;
+    }
+}
+
---
+++ My VLC Media Player/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace My_VLC_Media_Player
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine spEngine;
+        Choices commands;
+       
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+             spEngine = new SpeechRecognitionEngine();
+            commands = new Choices();
+            
+
+        }
+
+      
+
+        private void btnLoadFiles_Click(object sender, EventArgs e)
+        {
+
+            openFile();
+
+        }
+
+        private void btnPlayAll_Click(object sender, EventArgs e)
+        {
+            playAll();
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            string selectedFile = listView1.FocusedItem.SubItems[1].Text;
+            axWindowsMediaPlayer1.URL = @selectedFile;
+            this.mediaStatus.Text = axWindowsMediaPlayer1.currentMedia.name + " is Playing ...";
+        }
+
+        private  void  btnSpeechEnable_Click(object sender, EventArgs e)
+        {
+            commands.Add(new string[] { "open file","play next","play previous","play all" });
+            GrammarBuilder grmrBuilder = new GrammarBuilder(commands);
+            grmrBuilder.Append(grmrBuilder);
+            Grammar grammer = new Grammar(grmrBuilder);
+
+            try
+            {
+                spEngine.RequestRecognizerUpdate();
+                spEngine.LoadGrammarAsync(grammer);
+                spEngine.SetInputToDefaultAudioDevice();
+                spEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(SpeechRecognisedEvn);
+                // Wait LoadGrammer to be completed
+                //then start Speech Recognising
+                spEngine.LoadGrammarCompleted += new  EventHandler<LoadGrammarCompletedEventArgs>((s, evt) =>
+                {
+                   
+                    spEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+                    ////disable the speech command btn
+                    //this.btnSpeechEnable.Enabled = false;
+                });
+
+               
+            }
+            catch (Exception ex)
+            {
+
+               MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+
+        #region My handlers
+
+        private void playAll()
+        {
+
+            WMPLib.IWMPPlaylist playList = axWindowsMediaPlayer1.playlistCollection.newPlaylist("myPlayList");
+            WMPLib.IWMPMedia media;
+
+
+            try
+            {
+
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    int ii = 1;
+
+                    // creat media instance from the current file location
+                    media = axWindowsMediaPlayer1.newMedia(listView1.Items[i].SubItems[ii].Text);
+
+                    // add the media to playlist
+                    playList.appendItem(media);
+
+                    axWindowsMediaPlayer1.currentPlaylist = playList;
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                    axWindowsMediaPlayer1.PlayStateChange += (s ,e)=>
+                    {
+                        if (axWindowsMediaPlayer1.playState==WMPLib.WMPPlayState.wmppsPlaying)
+                        {
+                            this.mediaStatus.Text = axWindowsMediaPlayer1.currentMedia.name + " is Playing ...";
+                        }
+                        
+                    };
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't Play the Media", "Error");
+            }
+
+        }
+
+        private void openFile()
+        {
+            // open dialoge and select file 
+            Stream myStream = null;
+            OpenFileDialog opf = new OpenFileDialog();
+            //opf.InitialDirectory = @"C:\";
+            opf.Filter = "MP3 Audio File (*.mp3)|*.mp3| Windows Media File (*.wma)|*.wma|WAV Audio File  (*.wav)|*.wav|All FILES (*.*)|*.*";
+
+            opf.FilterIndex = 1;
+            opf.RestoreDirectory = true;
+            opf.Multiselect = true;
+
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = opf.OpenFile()) != null)
+                    {
+
+                       
+
+                        using (myStream)
+                        {
+                            // clear previous loaded files
+                            this.listView1.Items.Clear();
+
+                            string[] fileAndPathNames = opf.FileNames;
+                            string[] fileNames = opf.SafeFileNames;
+                            for (int i = 0; i < fileNames.Count(); i++)
+                            {
+
+                                string[] listViewItems = new string[2];
+                                listViewItems[0] = fileNames[i];
+                                listViewItems[1] = fileAndPathNames[i];
+
+                                ListViewItem lVItem = new ListViewItem(listViewItems);
+                                this.listView1.Items.Add(lVItem);
+
+                                
+
+
+                            }
+
+                        }
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Sorry Can't Open the file", "Error ");
+                }
+            }
+        }
+
+        // Speech Recognised Event
+          void SpeechRecognisedEvn(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speechText = e.Result.Text.ToLower();
+
+            if (e.Result.Text.ToLower().Contains("open") && speechText.Contains("file"))
+            {
+                openFile();
+            }
+            else if((speechText.Contains("play")&& speechText.Contains("next"))||speechText.Contains("next"))
+                { 
+                    playNext();
+                 }
+            else if ((speechText.Contains("play") && speechText.Contains("previous")) || speechText.Contains("previous"))
+            {
+                playPrevious();
+            }
+            else if ((speechText.Contains("play") && speechText.Contains("all")))
+            {
+                playAll();
+            }
+            else
+            {
+                MessageBox.Show(speechText.ToString() + " is not Speech Command");
+            }
+            
+
+
+           
+        }
+
+
+        private void playNext()
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.next();
+        }
+
+        private void playPrevious()
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.previous();
+        }
+
+
+        #endregion
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This App is for College Assessment developed by Samuel", "About");
+        }
+
+       
+    }
+
+
+    
+}
